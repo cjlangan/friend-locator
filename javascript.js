c411536c7dfa8eed6c4e334171a8dbf544e5c21e
cjlangan <connorjameslangan@gmail.com>
2024-10-25 23:15:53 -0500

    Location now automatically updates, although slow and potentially inaccurate

--- javascript.js
+++ javascript.js
@@ -1,17 +1,31 @@
-const latitude = document.getElementById("latitude");
-const longitude = document.getElementById("longitude");
+const lat_html = document.getElementById("latitude");
+const long_html = document.getElementById("longitude");
+const acc_html = document.getElementById("accuracy");
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: Infinity,
+    maximumAge: 0,
+};
 
 if("geolocation" in navigator)
 {
-    navigator.geolocation.getCurrentPosition((position) =>
-    {
-        latitude.innerHTML = "Latitude: " + position.coords.latitude
-        longitude.innerHTML = "Longitude: " + position.coords.longitude
-    });
+    navigator.geolocation.watchPosition(setLocation, error, options);
 }
 else
 {
-    console.log("Gelocation API is not available")
+    console.log("Gelocation API is not available");
 }
 
+function error(err)
+{
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
 
+function setLocation(position)
+{
+    lat_html.innerHTML = "Latitude: " + position.coords.latitude;
+    long_html.innerHTML = "Longitude: " + position.coords.longitude;
+    acc_html.innerHTML = "Accuracy: " + position.coords.accuracy;
+    console.log("Location set");
+}
