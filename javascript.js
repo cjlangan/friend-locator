05c36a7b90cff5f7f3364f198576917dee5c7d99
cjlangan <connorjameslangan@gmail.com>
2024-10-26 21:28:21 -0500

    Removed unnecary options from geolocation API

--- javascript.js
+++ javascript.js
@@ -2,24 +2,18 @@ const lat_html = document.getElementById("latitude");
 const long_html = document.getElementById("longitude");
 const acc_html = document.getElementById("accuracy");
 
-const options = {
-    enableHighAccuracy: true,
-    timeout: Infinity,
-    maximumAge: 0,
-};
+main()
 
-if("geolocation" in navigator)
+funciton main()
 {
-    navigator.geolocation.watchPosition(setLocation, error, options);
-}
-else
-{
-    console.log("Gelocation API is not available");
-}
-
-function error(err)
-{
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+    if("geolocation" in navigator)
+    {
+        navigator.geolocation.watchPosition(setLocation);
+    }
+    else
+    {
+        console.log("Gelocation API is not available");
+    }
 }
 
 function setLocation(position)
