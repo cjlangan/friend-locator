e4ba881b70f937008266141585c7c0c3074246bf
Logan <logand007@toulook.com>
2024-10-28 16:23:53 -0500

    Changed 'send_location' to 'send_post' be more modular

--- static/javascript.js
+++ static/javascript.js
@@ -140,28 +140,47 @@ function requestOrientationPermission()
 // Update html to reflect coordinates
 function setLocation(position)
 {
+    coords = position.coords;
     //sometimes the event triggers without the latitude or longitude changing.
     if(position.coords.latitude == myLatitude &&
        position.coords.longitude == myLongitude)
         return;
 
-    send_location(position.coords);
-    myLatitude = position.coords.latitude;
-    myLongitude = position.coords.longitude;
+    myLatitude = coords.latitude;
+    myLongitude = coords.longitude;
+
+    //send position to server.
+    position_array = ["lat", myLatitude, "lon", myLongitude, "acc", coords.accuracy]
+    send_post("/API/location", http_encode(position_array));
 
     lat_html.innerHTML = "Latitude: " + myLatitude;
     long_html.innerHTML = "Longitude: " + myLongitude;
-    acc_html.innerHTML = "Accuracy: " + position.coords.accuracy;
+    acc_html.innerHTML = "Accuracy: " + coords.accuracy;
     console.log("Location set");
 }
 
-function send_location(coords)
+//HTTP encodes an arry where the first element is the key and the second is the 
+//value, the third is the key and the fourth is the value and so on.
+function http_encode(array) {
+
+    if(array.length % 2 != 0)
+        return undefined;
+
+    let string = "";
+    for(i = 0; i < array.length / 2; i++) {
+        string += array[i] + "=" + array[i + 1];
+        if(i != array.length / 2 - 1)
+            string += "&";
+    }
+    return string;
+}
+
+function send_post(location, payload)
 {
     request = new XMLHttpRequest();     
-    request.open("POST", "/API/location");
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    body = 'lat='+coords.latitude+'&lon='+coords.longitude+'&acc='+coords.accuracy;
-    request.send(body)
+    request.open("POST", location);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send(payload)
 }
 
 
@@ -176,11 +195,11 @@ function handleOrientation(event)
 
     console.log("Orientation Received.")
 
-    rotation.innerHTML = "Rotation: " + compass;
+    rotation.innerHTML = "Rotation: " + rotateDegrees;
     left_to_right.innerHTML = "Left to Right: " + leftToRight;
     front_to_back.innerHTML = "Front to Back: " + frontToBack;
 
-    let angleStationary = findStationaryAngle(50, -100);
+    let angleStationary = findStationaryAngle(49, -200);
     let vectorAngle = angleStationary + compass;
 
     xFactor = Math.cos(vectorAngle * Math.PI / 180);
