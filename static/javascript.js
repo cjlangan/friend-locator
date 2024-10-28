2173390e874823d93d77cc27fd0ce9ba4b246f67
Logan <logand007@toulook.com>
2024-10-28 17:06:01 -0500

    commit

--- static/javascript.js
+++ static/javascript.js
@@ -19,6 +19,7 @@ let size; // dimension size of canvas
 
 let myLatitude;
 let myLongitude;
+let myOrientation;
 
 // Coordinate direction of vector. Range [-1, 1]. e.g: (1, -1) is the bottom right corner.
 let xFactor = 0;
