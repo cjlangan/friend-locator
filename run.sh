3dc76be1a96dcfd420bf5b5072d6a9799b3b3176
cjlangan <connorjameslangan@gmail.com>
2024-11-21 19:00:13 -0600

    Changed method nav bar options are spaced to account for older browsers, and option server runnning line

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 export FLASK_APP=main.py
 flask run --host=0.0.0.0
+
+# For running on server:
+#python3 -m gunicorn -w 4 -b 127.0.0.1:5000 main:app
+
+# Then I'm using apache2 to proxy pass 127.0.0.1:5000 to an https connection
