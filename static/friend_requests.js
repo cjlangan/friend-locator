3d782af5f0ee1142ccce46fb346341602dc21131
Logan <logand007@outlook.com>
2025-01-09 11:17:30 -0600

    minor

--- static/friend_requests.js
+++ static/friend_requests.js
@@ -1,7 +1,5 @@
 main()
 
-textbox = document.getElementById("username");
-
 async function get_requests(incoming=true) {
     path = "/API/friend_requests/" + (incoming ? "incoming" : "outgoing")
 
@@ -16,42 +14,6 @@ async function get_requests(incoming=true) {
 
 }
 
-
-//HTTP encodes an arry where the first element is the key and the second is the 
-//value, the third is the key and the fourth is the value and so on.
-function http_encode(array) {
-
-    if(array.length % 2 != 0)
-        return undefined;
-
-    let string = "";
-    for(let i = 0; i < array.length; i += 2) {
-        string += array[i] + "=" + array[i + 1];
-        if(i < array.length - 2)
-            string += "&";
-    }
-    return string;
-}
-
-
-// Function to simplify the process of sending a post request to the server
-function send_post(location, payload)
-{
-    request = new XMLHttpRequest();     
-    request.open("POST", location);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.send(payload)
-}
-
-
-async function send_friend_request() {
-    let name_array = ["name", textbox.value];
-    console.log(textbox.value);
-    console.log(http_encode(name_array));
-    send_post("/API/friend", http_encode(name_array));
-}
-
-
 function create_incoming_request_card(name) {
     card = document.createElement("div")
     card.classList.add("request_card")
@@ -60,8 +22,8 @@ function create_incoming_request_card(name) {
     textbox.style.height= "90%";
     textbox.style.border= "dotted";
     card.appendChild(textbox);
-    accept_button = document.createElement("div")
-    reject_button = document.createElement("div")
+    accept_button = document.createElement("button")
+    reject_button = document.createElement("button")
     accept_button.classList.add("request_button")
     reject_button.classList.add("request_button")
     button_text_box1 = document.createElement("div")
@@ -97,18 +59,6 @@ function get_requests(container) {
     }
     req.send()
 }
-
-submit_if_enter = function (e) 
-{
-    if(e.key == 'Enter')
-    {
-        send_friend_request();
-    }
-}
-
-window.addEventListener('keypress', submit_if_enter)
-
-
 
 
 function main() {
